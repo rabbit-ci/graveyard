e3d7ae9991af312e56533df31bd443b9b292828a
Theron Boerner <hunterboerner@gmail.com>
2015-04-11 14:32:50 -0500

    Early prototype

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func myFunc(queue string, args ...interface{}) error {
 	out, err := exec.Command("worker", "extract-file",
 		"git@github.com:hunterboerner/rabbitci-test-repo.git",
-		"ebd9c8282f992e009e04dcbf6c1e7df07f554c48").Output()
+		"ebd9c8282f992e009e04dcbf6c1e7df07f554c48", "--file=.rabbitci.json").Output()
 	fmt.Println(err)
 	fmt.Println(string(out))
 	fmt.Printf("From %s, %v\n", queue, args)
