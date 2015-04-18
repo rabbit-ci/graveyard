4f499363ef7f4e99f71335b61b06f024e1927ef9
Theron Boerner <hunterboerner@gmail.com>
2015-04-18 18:17:49 -0500

    Scripts!

--- build_runner.go
+++ build_runner.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"os/exec"
 	"time"
+	"github.com/antonholmquist/jason"
 )
 
 func buildRunner(queue string, args ...interface{}) error {
@@ -25,10 +26,11 @@ func buildRunner(queue string, args ...interface{}) error {
 	projectName := args[0].(string)
 	branchName := args[1].(string)
 	buildNumber := string(args[2].(json.Number))
+	scriptName := args[3].(string)
 
 	protocol := "http://"
 	authority := "localhost:4000"
-	url := fmt.Sprintf("%v%v/projects/%v/branches/%v/builds/%v/config_file",
+	url := fmt.Sprintf("%v%v/projects/%v/branches/%v/builds/%v/config",
 		protocol, authority, projectName, branchName, buildNumber)
 	req, _ := http.NewRequest("GET", url, nil)
 	client := &http.Client{}
@@ -46,25 +48,40 @@ func buildRunner(queue string, args ...interface{}) error {
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 
-	var configJson map[string]interface{}
-	if err := json.Unmarshal(body, &configJson); err != nil {
-		return err
-	}
+	// var configJson map[string]interface{}
+	// if err := json.Unmarshal(body, &configJson); err != nil {
+	// 	return err
+	// }
 
+	configJson, err := jason.NewObjectFromBytes(body)
+	
 	var buffer bytes.Buffer
-	for key, value := range configJson["ENV"].(map[string]interface{}) {
-		buffer.WriteString(fmt.Sprintf("export %v=\"%v\"\n", key,
-			value))
+
+	index := 0
+	scriptMap, _ := configJson.GetObjectArray("scripts")
+	for _, script := range scriptMap {
+		name, _ := script.GetString("name")
+		if name == scriptName {
+			break
+		}
+		index++
 	}
 
-	for _, cmd := range configJson["commands"].([]interface{}) {
+	envMap, _ := scriptMap[index].GetObject("ENV")
+	for key, value := range envMap.Map() {
+		value_str, _ := value.String()
+		buffer.WriteString(fmt.Sprintf("export %v=\"%v\"\n", key,
+			value_str))
+	}
+
+	commands, _ := scriptMap[index].GetStringArray("commands")
+	for _, cmd := range commands {
 		buffer.WriteString(fmt.Sprintf("echo '=== Running command: %v'\n",
-			cmd.(string)))
-		buffer.WriteString(fmt.Sprintf("%v\n", cmd.(string)))
+			cmd))
+		buffer.WriteString(fmt.Sprintf("%v\n", cmd))
 	}
 
 	dir, err := ioutil.TempDir("", "buildRunner")
-	fmt.Println(dir)
 	defer os.RemoveAll(dir)
 
 	if err != nil {
