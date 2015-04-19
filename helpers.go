cfeebbd1d25b1017035c204b0f91c39d2d47d807
Theron Boerner <hunterboerner@gmail.com>
2015-04-19 12:15:56 -0500

    Command runner :)
    Logstreamer usage was brutally ripped out of this blog post:
    http://kvz.io/blog/2013/07/12/prefix-streaming-stdout-and-stderr-in-golang/
    The project was forked and can be found at http://github.com/rabbit-ci/logstreamer

--- helpers.go
+++ helpers.go
@@ -1,11 +1,57 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"github.com/rabbit-ci/logstreamer"
+	"io"
+	"log"
+	"os/exec"
+	"time"
 )
 
 func timeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
 	fmt.Printf("%s took %s\n", name, elapsed)
+}
+
+type CommandLog struct {
+	buffer   *io.PipeReader
+	finished bool
+}
+
+func runCommand(name string, arg ...string) *CommandLog {
+	// var b bytes.Buffer
+	pipeReader, pipeWriter := io.Pipe()
+	// writer := bufio.NewWriter(&b)
+	logger := log.New(pipeWriter, "--> ", log.Ldate|log.Ltime)
+
+	// Setup a streamer that we'll pipe cmd.Stdout to
+	logStreamerOut := logstreamer.NewLogstreamer(logger, "stdout", false)
+	// Setup a streamer that we'll pipe cmd.Stderr to.
+	// We want to record/buffer anything that's written to this (3rd argument true)
+	logStreamerErr := logstreamer.NewLogstreamer(logger, "stderr", true)
+
+	cmd := exec.Command(name, arg...)
+	cmd.Stderr = logStreamerErr
+	cmd.Stdout = logStreamerOut
+
+	// Reset any error we recorded
+	logStreamerErr.FlushRecord()
+
+	cl := CommandLog{buffer: pipeReader, finished: false}
+	go func() {
+		err := cmd.Start()
+		// Failed to spawn?
+		if err != nil {
+			logger.Printf("ERROR could not spawn command. %s \n", err.Error())
+		}
+		errf := cmd.Wait()
+		if errf != nil {
+			logger.Printf("Someting went wrong. %s \n", err.Error())
+		}
+		cl.finished = true
+		logger.Print("Finished")
+	}()
+
+	return &cl
 }
---
+++ helpers_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRunCommand(t *testing.T) {
+	cl := runCommand("sh", "-c", "echo 'wut'; sleep 2; echo 'hey'; echo 'wat'; sleep 2; echo 'finish'")
+	pipeReader := cl.buffer
+	buffer := make([]byte, 1024)
+	for !cl.finished {
+		n, err := pipeReader.Read(buffer)
+		if err != nil {
+			pipeReader.Close()
+			break
+		}
+
+		data := buffer[0:n]
+		fmt.Print(string(data))
+		for i := 0; i < n; i++ {
+			buffer[i] = 0
+		}
+	}
+}
